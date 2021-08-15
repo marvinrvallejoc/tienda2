3a0ab636c0b4b2be4408f3499adcb539e8bf094a
marvinrvallejoc <est.mvallejo@uml.edu.ni>
2021-08-15 10:31:27 -0600

    version 2 del proyecto

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Somomos </br>
+                    Nuestra misión es permitir a las personas.</br>
+                    crear la tecnología que sea accesibles.</br>
+                    para todas las edades.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Honduras</p>
+                <p style="color: #FFF">El Paraiso,El Paraiso</p>
+                <p style="font-size:20px" aria-hidden="true">  calle 3 / Barrio las lomitas</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">95219751 / 95309193 <a href="#" target="_blank" style="color: #8BB7EA ">envianos un mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #8BB7EA "> &nbsp; est.mvallejo@uml.edu.ni</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Variedades DANNA &copy; 2021</h5>
+</footer>
